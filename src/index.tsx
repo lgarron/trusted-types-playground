b4d67f5d0ed16bc50d25e8d0b12eb5d05a131488
Lucas Garron <lgarron@github.com>
2022-05-11 15:28:09 -0700

    Add non-working module override.

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,17 @@ const root = createRoot(
   document.getElementById('root')
 );
 
+// This doesn't work. 😢
+declare module "react" {
+   namespace React {
+    interface DOMAttributes<T> {
+      dangerouslySetInnerHTML?: {
+        __html: string | TrustedHTML
+      } | undefined;
+    }
+  }
+}
+
 const trustedHTML = DOMPurify.sanitize("<i>Hello 1 World! <input value='hi 1'></input></i>", { RETURN_TRUSTED_TYPE: true });
 const element = <h1 dangerouslySetInnerHTML={{__html: trustedHTML}}></h1>;
 root.render(element);
