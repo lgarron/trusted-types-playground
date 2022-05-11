1ed9f30481e98a0f9da29898ceb2a0c213028736
Lucas Garron <lgarron@github.com>
2022-05-11 15:27:23 -0700

    Split up cases.

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,15 @@ const root = createRoot(
   document.getElementById('root')
 );
 
-const trustedHTML = DOMPurify.sanitize("<i>Hello!<input value='hi'></input></i>", { RETURN_TRUSTED_TYPE: true });
+const trustedHTML = DOMPurify.sanitize("<i>Hello 1 World! <input value='hi 1'></input></i>", { RETURN_TRUSTED_TYPE: true });
 const element = <h1 dangerouslySetInnerHTML={{__html: trustedHTML}}></h1>;
 root.render(element);
 
-const trustedHTML2 = DOMPurify.sanitize("<i>Hello!<input value='hi'></input></i>", { RETURN_TRUSTED_TYPE: true });
-const div = document.body.appendChild(document.createElement("div"));
-div.innerHTML = trustedHTML2;
-div.innerHTML = "<i>Hello!<input value='hi'></input></i>";
+const trustedHTML2 = DOMPurify.sanitize("<i>Hello 2 World! <input value='hi 2'></input></i>", { RETURN_TRUSTED_TYPE: true });
+const div2 = document.body.appendChild(document.createElement("div"));
+div2.innerHTML = trustedHTML2;
+
+const div3 = document.body.appendChild(document.createElement("div"));
+div3.innerHTML = "<i>Hello 3 World! <input value='hi 3'></input></i>";
 
 // console.log()
