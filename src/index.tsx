3eaeaaee7a1741ba8631eb788bd9193658b535c5
Lucas Garron <lgarron@github.com>
2022-10-20 13:34:59 -0700

    Update example.

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,12 @@
+
+import("./react");
 import DOMPurify from 'dompurify';
-import { createRoot } from "react-dom/client";
 
-const root = createRoot(
-  document.getElementById('root')
-);
-
-// This doesn't work. 😢
-declare module "react" {
-   namespace React {
-    interface DOMAttributes<T> {
-      dangerouslySetInnerHTML?: {
-        __html: string | TrustedHTML
-      } | undefined;
-    }
-  }
-}
-
-const trustedHTML = DOMPurify.sanitize("<i>Hello 1 World! <input value='hi 1'></input></i>", { RETURN_TRUSTED_TYPE: true });
-const element = <h1 dangerouslySetInnerHTML={{__html: trustedHTML}}></h1>;
-root.render(element);
+// declare global {
+//   interface InnerHTML {
+//     set innerHTML(v: string | TrustedHTML)
+//   }
+// }
 
 const trustedHTML2 = DOMPurify.sanitize("<i>Hello 2 World! <input value='hi 2'></input></i>", { RETURN_TRUSTED_TYPE: true });
 const div2 = document.body.appendChild(document.createElement("div"));
@@ -27,4 +15,3 @@ div2.innerHTML = trustedHTML2;
 const div3 = document.body.appendChild(document.createElement("div"));
 div3.innerHTML = "<i>Hello 3 World! <input value='hi 3'></input></i>";
 
-// console.log()
